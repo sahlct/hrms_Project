e376b226fea50b28df26825004c5ab821f3dc872
sahlct <mhdsahlct@gmail.com>
2024-04-29 12:41:18 +0530

    forget pass page added

--- src/components/homePage/home.jsx
+++ src/components/homePage/home.jsx
@@ -4,14 +4,23 @@ import './home.css';
 
 export default function Home() {
   const [isSignUp, setIsSignUp] = useState(true);
+  const [reset, setReset] = useState(false);
 
-  function toggleForm() {
+  function toggleForm(data) {
     setIsSignUp(prevState => !prevState);
+    if(data === true){
+        setReset(true)
+    }else{
+        setReset(false)
+    }
   }
   return (
     <div className="mainContainer">
     <div className={`containerLogin ${isSignUp ? 'active' : ''}`}>
-      <div className="form-container sign-up">
+
+        {/* creat Account  */}
+
+      <div style={{display: reset ? 'none' : 'flex'}} className="form-container sign-up">
         <form action='/signup'>
         <div className='headings d-flex'>
           <h1 className='validText'>Create Account</h1>
@@ -27,7 +36,10 @@ export default function Home() {
             <p className='hiddenText bottomText'>You have already an account <span style={{color:'blue', cursor:'pointer'}} onClick={toggleForm}>Login</span></p>
         </form>
       </div>
-      <div className="form-container sign-in">
+
+            {/* Login Page  */}
+
+      <div style={{display: reset ? 'none' : 'flex'}} className="form-container sign-in">
         <form action='/login'>
           <div className='headings d-flex'><h1 className='hiddenText' onClick={toggleForm}>Create Account</h1>
           <h1 className='validText'>Login</h1></div>
@@ -37,25 +49,58 @@ export default function Home() {
           <input type="email" placeholder="Email" required/>
           <input type="password" placeholder="Password" required/>
           </div>
-          <p className='bottomText'>Forgot your Password ? <span className='text-decoration-underline' style={{color:'blue', cursor:'pointer'}}>Forgot</span></p>
+          <p className='bottomText'>Forgot your Password ? <span className='text-decoration-underline' style={{color:'blue', cursor:'pointer'}} onClick={()=> toggleForm(true)}>Forgot</span></p>
           {/* <p style={{margin:'0 0 20px 0'}} className='hiddenText bottomText hideContent'>You don't have an account <span style={{color:'blue', cursor:'pointer'}} onClick={toggleForm}>Create account</span></p> */}
           <button type="submit">Login</button>
         </form>
       </div>
+
+                {/* forget Password */}
+
+      <div style={{display: reset ? 'flex' : 'none'}} className="form-container forget">
+        <form action='/forget'>
+        <div className='headings d-flex'>
+          <h1 className='validText'>Reset Pass</h1>
+          {/* <h1 className='hiddenText' onClick={toggleForm}>Forget Password</h1> */}
+        </div>
+          <span className='regText'>Use your email for reset password</span>
+          <div className='inputContainer'>
+          <input type="email" placeholder="Email" required/>
+          </div>
+          <button type="submit">Reset Password</button>
+            <p className='hiddenText bottomText'>You have already an account <span style={{color:'blue', cursor:'pointer'}} onClick={toggleForm}>Login</span></p>
+        </form>
+      </div>
       <div className="toggle-container">
         <div className="toggle">
-          <div className="toggle-panel toggle-left">
+
+                {/* create Ac Left  */}
+
+          <div style={{display: reset ? 'none' : 'flex'}} className="toggle-panel toggle-left">
             <h1>Welcome Back!</h1>
             <p>Enter your personal details to use all of site features</p>
             <button className="hidden" onClick={toggleForm}>
               Login
             </button>
           </div>
-          <div className="toggle-panel toggle-right">
+
+                    {/* login page left  */}
+
+          <div style={{display: reset ? 'none' : 'flex'}} className="toggle-panel toggle-right">
             <h1>Hello Friend!</h1>
             <p>Register with your personal details to use all of site features</p>
             <button className="hidden" onClick={toggleForm}>
               Sign Up
+            </button>
+          </div>
+
+                    {/* forget Pass left  */}
+
+          <div style={{display: reset ? 'flex' : 'none'}}  className="toggle-panel toggle-left ">
+            <h1>Forgot Password</h1>
+            <p>Enter your personal details to use all of site features</p>
+            <button className="hidden" onClick={()=> toggleForm(false)}>
+              Login
             </button>
           </div>
         </div>
