9610f956fb7906d230ef61333c04ec92b43d2d11
sahlct <mhdsahlct@gmail.com>
2024-05-07 18:48:56 +0530

    sidebar designed normally & pending accordion

--- src/components/landingPage/container.jsx
+++ src/components/landingPage/container.jsx
@@ -1,19 +1,30 @@
-import Navbar from './navbar'
-import OffCanvas from './offCanvas'
-import SmallCard from './smallCard'
-import MainCard from './mainCard'
-import { useState } from 'react'
+import Navbar from "./navbar";
+import OffCanvas from "./offCanvas";
+import SmallCard from "./smallCard";
+import MainCard from "./mainCard";
+import { useState } from "react";
 
 export default function Container() {
-    const [width, setWidth] = useState(false)
+  const [width, setWidth] = useState(false);
+  const [canvasWidth, setCanvasWidth] = useState('80px');
+  const [display, setDisplay] = useState('0%')
   return (
-    <div className='home_mainContainer  d-flex justify-content-end' style={{minHeight:'100vh'}}>
-        <Navbar setWidth={setWidth} width={width} />
-        <OffCanvas />
-        <div className="semi_container" style={{ width: width ? 'calc(100vw - 400px)' : '100%', transition:'0.6s ease-in-out' }}>
+    <div
+      className="home_mainContainer  d-flex justify-content-end"
+      style={{ minHeight: "100vh" }}
+    >
+      <Navbar setWidth={setWidth} width={width} setCanvasWidth={setCanvasWidth} setDisplay={setDisplay}/>
+      <OffCanvas canvasWidth={canvasWidth} display={display}/>
+      <div
+        className="semi_container"
+        style={{
+          width: width ? "calc(100vw - 300px)" : "95%",
+          transition: "0.6s ease-in-out",
+        }}
+      >
         <SmallCard />
         <MainCard />
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/landingPage/navbar.jsx
+++ src/components/landingPage/navbar.jsx
@@ -1,37 +1,88 @@
-import React from 'react'
+import React from "react";
 // import OffCanvas from './offCanvas'
 
-export default function Navbar({width, setWidth}) {
-    
-    // setWidth(true)
-    function settingWidth(){
-       if(width === false){
-        setWidth(true)
-       }else{
-        setWidth(false)
-       }
+export default function Navbar({ width, setWidth, setCanvasWidth, setDisplay}) {
+  // setWidth(true)
+  function settingWidth() {
+    if (width === false) {
+      setWidth(true);
+      setCanvasWidth("250px")
+      setDisplay('100%')
+    } else {
+      setWidth(false);
+      setCanvasWidth("80px")
+      setDisplay('0%')
     }
+  }
   return (
-    <div style={{backgroundColor:'white', position:'fixed', top:'0', width:'100%'}}>
-        <nav  class="navbar" >
-  <div class="Maincontainer-fluid w-100 d-flex align-items-center flex-nowrap justify-content-between">
-    <div className="rightSide d-flex align-items-center ps-lg-5 ps-sm-1">
-    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" onClick={()=> settingWidth()}><i style={{fontSize:'25px'}} class="bi bi-list"></i></button>
-   
-    <h1 class="navbar-brand ms-lg-5 ms-sm-1 ">Circle <span style={{fontWeight:'700'}} >Soft</span></h1>
+    <div
+      style={{
+        backgroundColor: "rgb(239, 239, 239)",
+        position: "fixed",
+        top: "0",
+        width: "100%",
+        zIndex:'2'
+      }}
+    >
+      <nav class="navbar">
+        <div class="Maincontainer-fluid w-100 d-flex align-items-center flex-nowrap justify-content-between">
+          <div className="rightSide d-flex align-items-center ps-lg-4 ps-sm-1 justify-content-center ">
+            <button
+              class="btn"
+              type="button"
+              onClick={() => settingWidth()}
+            >
+              <i
+                style={{ fontSize: "25px", color: "black" }}
+                class="bi bi-list"
+              ></i>
+            </button>
+
+            <h1 class="navbar-brand ms-lg-5 ms-sm-1 d-flex align-items-center justify-content-center pb-0" style={{ color: "black", fontSize:'25px'}}>
+              Circle <span style={{ fontWeight: "700" }}>Soft</span>
+            </h1>
+          </div>
+          <div className="centerSide w-50">
+          <form className="d-flex w-100 " role="search">
+            <div className="searchBox d-flex align-items-center w-75 ">
+              {/* <div style={{color:'black'}}><i class="bi bi-search"></i></div> */}
+              <input
+                class="form-control  border-0 "
+                type="search"
+                placeholder="🔍   Search..."
+                aria-label="Search"
+              />
+              {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
+            </div>
+          </form>
+          </div>
+          <div className="rightSide">
+          <button
+            style={{
+              backgroundColor: "#f7f7f7",
+              borderRadius: "8px",
+              position: "relative",
+            }}
+            className="notificationBtn btn border-0 me-lg-5 me-sm-1 "
+          >
+            <i class="bi bi-bell"></i>
+            <div
+              style={{
+                position: "fixed",
+                width: "5px",
+                height: "5px",
+                backgroundColor: "red",
+                borderRadius: "50%",
+                top: "25px",
+                right: "62px",
+              }}
+            ></div>
+          </button>
+          </div>
+          {/* <div className="profile"></div> */}
+        </div>
+        {/* <OffCanvas/> */}
+      </nav>
     </div>
-    <form class="d-flex" role="search">
-    <div  className="searchBox d-flex align-items-center">
-      {/* <div style={{color:'black'}}><i class="bi bi-search"></i></div> */}
-      <input class="form-control  border-0 " type="search" placeholder="🔍   Search..." aria-label="Search"/>
-      {/* <button class="btn btn-outline-success" type="submit">Search</button> */}
-      </div>
-    </form>
-      <button style={{backgroundColor:'#f7f7f7', borderRadius:"8px", position:'relative'}} className='notificationBtn btn border-0 me-lg-5 me-sm-1 '><i class="bi bi-bell"></i>
-      <div style={{position:'fixed', width:'5px', height:'5px', backgroundColor:'red', borderRadius:'50%', top:'25px', right:'62px'}}></div></button>
-  </div>
-  {/* <OffCanvas/> */}
-</nav>
-    </div>
-  )
+  );
 }
