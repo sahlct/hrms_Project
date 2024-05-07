d990b63d2cf07301d43c2fa64cf4eafb48c5d3d0
sahlct <mhdsahlct@gmail.com>
2024-05-07 11:10:42 +0530

    offcanvas position currected

--- src/components/landingPage/container.jsx
+++ src/components/landingPage/container.jsx
@@ -7,10 +7,10 @@ import { useState } from 'react'
 export default function Container() {
     const [width, setWidth] = useState(false)
   return (
-    <div className='home_mainContainer  d-flex justify-content-end' style={{width:'100vw', minHeight:'100vh'}}>
+    <div className='home_mainContainer  d-flex justify-content-end' style={{minHeight:'100vh'}}>
         <Navbar setWidth={setWidth} width={width} />
         <OffCanvas />
-        <div className="semi_container" style={{ width: width ? 'calc(100vw - 400px)' : '100%' }}>
+        <div className="semi_container" style={{ width: width ? 'calc(100vw - 400px)' : '100%', transition:'0.6s ease-in-out' }}>
         <SmallCard />
         <MainCard />
         </div>
--- src/components/landingPage/navbar.jsx
+++ src/components/landingPage/navbar.jsx
@@ -16,7 +16,7 @@ export default function Navbar({width, setWidth}) {
         <nav  class="navbar" >
   <div class="Maincontainer-fluid w-100 d-flex align-items-center flex-nowrap justify-content-between">
     <div className="rightSide d-flex align-items-center ps-lg-5 ps-sm-1">
-    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i style={{fontSize:'25px'}} class="bi bi-list" onClick={()=> settingWidth()}></i></button>
+    <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling" onClick={()=> settingWidth()}><i style={{fontSize:'25px'}} class="bi bi-list"></i></button>
    
     <h1 class="navbar-brand ms-lg-5 ms-sm-1 ">Circle <span style={{fontWeight:'700'}} >Soft</span></h1>
     </div>
@@ -28,7 +28,7 @@ export default function Navbar({width, setWidth}) {
       </div>
     </form>
       <button style={{backgroundColor:'#f7f7f7', borderRadius:"8px", position:'relative'}} className='notificationBtn btn border-0 me-lg-5 me-sm-1 '><i class="bi bi-bell"></i>
-      <div style={{position:'fixed', width:'5px', height:'5px', backgroundColor:'red', borderRadius:'50%', top:'25px', right:'58px'}}></div></button>
+      <div style={{position:'fixed', width:'5px', height:'5px', backgroundColor:'red', borderRadius:'50%', top:'25px', right:'62px'}}></div></button>
   </div>
   {/* <OffCanvas/> */}
 </nav>
