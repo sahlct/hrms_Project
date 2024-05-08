e6b7bc2dfa2d9b494e2818fb438d643912d52d62
sahlct <mhdsahlct@gmail.com>
2024-05-09 01:15:06 +0530

    small card design changed

--- src/components/landingPage/smallCard.jsx
+++ src/components/landingPage/smallCard.jsx
@@ -2,39 +2,49 @@ import React from 'react'
 
 export default function SmallCard() {
   return (
-    <div className='d-flex justify-content-around flex-wrap row-gap-3' style={{marginTop:'67px', paddingTop:'30px'}}>
-        <div style={{maxWidth:'300px', minWidth:'250px', border:'1px solid #eceef6',height:'150px', padding:'30px 40px', backgroundColor:'white'}} className="smCard_Container d-flex flex-column align-items-start rounded-2">
-            <div className='d-flex gap-4'>
-                <h5 style={{fontSize:'15px', fontWeight:'700'}}>Total Employees</h5>
-                <div style={{backgroundColor:'#def6da', color:'#18910e', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>10.0 %</div>
-            </div>
-            <h2>856</h2>
-            <p style={{fontSize:'15px'}}>Employee</p>
-        </div>
-        {/* <div style={{maxWidth:'300px', minWidth:'250px',  border:'1px solid #eceef6',height:'150px', padding:'30px 40px', backgroundColor:'white'}} className="smCard_Container d-flex flex-column align-items-start rounded-2 ">
-            <div className='d-flex gap-4'>
+    <div class="container" style={{marginTop:'67px', paddingTop:'30px'}}>
+  <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-5 gap-4 justify-content-center" >
+  <div class="col rounded-3 p-0 overflow-hidden" style={{maxWidth:'280px', minWidth:'250px',border:'2px solid grey'}}>
+      <div class="ps-4 pt-3 pe-4 bg-light" style={{height:'120px'}}>
+      <div className='d-flex gap-4 justify-content-between '>
                 <h5 style={{fontSize:'15px', fontWeight:'700'}}>Job view</h5>
                 <div style={{backgroundColor:'#def6da', color:'#18910e', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>22.0 %</div>
             </div>
             <h2>3342</h2>
             <p style={{fontSize:'15px'}}>viewers</p>
-        </div> */}
-        {/* <div style={{maxWidth:'300px', minWidth:'250px',  border:'1px solid #eceef6',height:'150px', padding:'30px 40px', backgroundColor:'white'}} className="smCard_Container d-flex flex-column align-items-start rounded-2 ">
-            <div className='d-flex gap-4'>
+      </div>
+    </div>
+    <div class="col rounded-3 p-0 overflow-hidden" style={{maxWidth:'280px', minWidth:'250px',border:'2px solid grey'}}>
+      <div class="ps-4 pt-3 pe-4 bg-light" style={{height:'120px'}}>
+      <div className='d-flex gap-4 justify-content-between '>
                 <h5 style={{fontSize:'15px', fontWeight:'700'}}>Job Applied</h5>
                 <div style={{backgroundColor:'#def6da', color:'#18910e', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>12.0 %</div>
             </div>
             <h2>77</h2>
             <p style={{fontSize:'15px'}}>Applicants</p>
-        </div> */}
-        {/* <div style={{maxWidth:'300px', minWidth:'250px',  border:'1px solid #eceef6',height:'150px', padding:'30px 40px', backgroundColor:'white'}} className="smCard_Container d-flex flex-column align-items-start rounded-2 ">
-            <div className='d-flex gap-4'>
-                <h5 style={{fontSize:'15px', fontWeight:'700'}}>Resign Employee</h5>
-                <div style={{backgroundColor:'#f6dada', color:'#cd293c', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>7.0 %</div>
+      </div>
+    </div>
+    <div class="col rounded-3 p-0 overflow-hidden" style={{maxWidth:'280px', minWidth:'250px',border:'2px solid grey'}}>
+      <div class="ps-4 pt-3 pe-4 bg-light" style={{height:'120px'}}>
+      <div className='d-flex gap-4 justify-content-between '>
+                <h5 style={{fontSize:'15px', fontWeight:'700'}}>Hiring</h5>
+                <div style={{backgroundColor:'#def6da', color:'#18910e', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>48.0 %</div>
+            </div>
+            <h2>442</h2>
+            <p style={{fontSize:'15px'}}>viewers</p>
+      </div>
+    </div>
+    <div class="col rounded-3 p-0 overflow-hidden" style={{maxWidth:'280px', minWidth:'250px',border:'2px solid grey'}}>
+      <div class="ps-4 pt-3 pe-4 bg-light" style={{height:'120px'}}>
+      <div className='d-flex gap-4 justify-content-between '>
+                <h5 style={{fontSize:'15px', fontWeight:'700'}}>Resigns</h5>
+                <div style={{backgroundColor:'#def6da', color:'#18910e', fontWeight:'700', fontSize:'14px'}} className='percentage ps-2 pe-2 rounded-4 d-flex align-items-center '>7.0 %</div>
             </div>
             <h2>17</h2>
-            <p style={{fontSize:'15px',}}>Employee</p>
-        </div> */}
+            <p style={{fontSize:'15px'}}>Employee</p>
+      </div>
     </div>
+  </div>
+     </div>
   )
 }
