6855ae426cd1f0f0fb1f75987a74ee75f9ebe48f
sahlct <mhdsahlct@gmail.com>
2024-05-14 12:42:16 +0530

    leav display changed

--- src/components/landingPage/document.jsx
+++ src/components/landingPage/document.jsx
@@ -6,19 +6,19 @@ export default function Document() {
     <div className='documentContainer rounded-3 p-3' style={{backgroundColor:'#f1f1ff'}}>
         <div>
             <h5 className=' mb-3 ms-3 '>Documents</h5>
-            <p className='text-black d-flex gap-2 align-items-center pointer' style={{fontWeight:'500'}}>
+            <p className='text-black d-flex align-items-center pointer' style={{fontWeight:'500'}}>
                 <i class="lni lni-files" style={{fontSize:'25px'}}></i> signed Documents 
                 <span style={{fontWeight:'500',color:'grey'}}>{count + 10}</span>
             </p>
-            <p className='text-black d-flex gap-2 align-items-center' style={{fontWeight:'500'}}>
+            <p className='text-black d-flex align-items-center' style={{fontWeight:'500'}}>
                 <i class="lni lni-files" style={{fontSize:'25px'}}></i> Tasklist Attachments 
                 <span style={{fontWeight:'500',color:'grey'}}>{count}</span>
             </p>
-            <p className='text-black d-flex gap-2 align-items-center' style={{fontWeight:'500'}}>
+            <p className='text-black d-flex align-items-center' style={{fontWeight:'500'}}>
                 <i class="lni lni-files" style={{fontSize:'25px'}}></i> Workflow Attachments 
                 <span style={{fontWeight:'500',color:'grey'}}>{count + 3}</span>
             </p>
-            <p className='text-black d-flex gap-2 align-items-center' style={{fontWeight:'500'}}>
+            <p className='text-black d-flex align-items-center' style={{fontWeight:'500'}}>
                 <i class="lni lni-files" style={{fontSize:'25px'}}></i> Non-disclosure Documents 
                 <span style={{fontWeight:'500',color:'grey'}}>{count - 2}</span>
             </p>
--- src/components/landingPage/leftContainer.jsx
+++ src/components/landingPage/leftContainer.jsx
@@ -8,7 +8,7 @@ export default function LeftContainer() {
   return (
     <div class="leftcontainer d-flex gap-5 ps-0 pe-0" style={{marginTop:'68px', paddingTop:'20px'}}>
       <div className="fullContainer d-flex flex-column ps-4 w-100 row-gap-4">
-      <div class="row me-0 mb-0 overflow-auto me-4" style={{paddingLeft:'24px'}}>
+      <div class="row me-0 mb-0 overflow-auto me-4" style={{paddingLeft:'20px'}}>
         <div
           class="col p-0 overflow-hidden d-flex align-items-center"
           style={{
@@ -20,10 +20,10 @@ export default function LeftContainer() {
             // boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 5px 0px'
           }}
         >
-          <div className="IconParent h-100  d-flex" style={{width:'30%'}}>
+          <div className="IconParent h-100  d-flex">
             <div className="mainIcon d-flex justify-content-center align-items-center" style={{backgroundColor:'#eaedff'}}><i class="bi bi-people" style={{fontSize:'25px', color:'blue'}}></i></div>   
           </div>
-          <div className="textHead d-flex h-100 gap-1"  style={{width:'70%'}}>
+          <div className="textHead d-flex h-100 gap-1">
             <div className="headerText" style={{ fontSize: "clamp(12px, 1.2vw, 17px)", fontWeight: "700" }} >Total Employees</div>
             <div className="countText" style={{ fontSize: "clamp(20px, 2vw, 30px)", fontWeight: "700"}}>735</div>
           </div>
@@ -39,10 +39,10 @@ export default function LeftContainer() {
             // boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 5px 0px'
           }}
         >
-          <div className="IconParent h-100  d-flex" style={{width:'30%'}}>
+          <div className="IconParent h-100  d-flex">
             <div className="mainIcon d-flex justify-content-center align-items-center" style={{backgroundColor:'#e3fdd9' }}><i class="bi bi-person-check" style={{fontSize:'25px', color:'green'}}></i></div>   
           </div>
-          <div className="textHead d-flex  h-100 gap-1"  style={{width:'70%'}}>
+          <div className="textHead d-flex  h-100 gap-1">
             <div className="headerText" style={{ fontSize: "clamp(12px, 1.2vw, 17px)", fontWeight: "700" }} >Toady's Attendence</div>
             <div className="countText" style={{fontSize: "clamp(20px, 2vw, 30px)", fontWeight: "700"}}>97%</div>
           </div>
@@ -58,10 +58,10 @@ export default function LeftContainer() {
             // boxShadow:'rgba(100, 100, 111, 0.2) 0px 7px 5px 0px'
           }}
         >
-          <div className="IconParent h-100  d-flex" style={{width:'30%'}}>
+          <div className="IconParent h-100  d-flex">
             <div className="mainIcon d-flex justify-content-center align-items-center" style={{backgroundColor:'#fde9cf'}}><i class="bi bi-person-x" style={{fontSize:'25px', color:'orange'}}></i></div>   
           </div>
-          <div className="textHead d-flex h-100 gap-1"  style={{width:'70%'}}>
+          <div className="textHead d-flex h-100 gap-1" >
             <div className="headerText" style={{  fontSize: "clamp(12px, 1.2vw, 17px)", fontWeight: "700" }} >Leave Employees</div>
             <div className="countText" style={{fontSize: "clamp(20px, 2vw, 30px)", fontWeight: "700"}}>12</div>
           </div>
