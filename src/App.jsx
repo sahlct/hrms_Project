b123331941819bc5bd9df71ab7fe18e51648b493
sahlct <mhdsahlct@gmail.com>
2024-05-14 09:31:56 +0530

    just route change for mobile testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes} from "react-router-dom";
 import About from "./components/aboutPage/about";
 import Registration from "./components/RegPage/registration";
-import Login from "./components/loginPage/login";
-import Signup from "./components/signupPage/signUp";
+// import Login from "./components/loginPage/login";
+// import Signup from "./components/signupPage/signUp";
 import './App.css';
 import Home from "./components/homePage/home";
 import Container from "./components/landingPage/container";
@@ -12,8 +12,8 @@ function App() {
     <div className="App" style={{backgroundColor:'#f8fafb'}}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Container />} />
+        <Route path="/signup" element={<Container />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/about" element={<About />} />
         <Route path="/home" element={<Container />} />
